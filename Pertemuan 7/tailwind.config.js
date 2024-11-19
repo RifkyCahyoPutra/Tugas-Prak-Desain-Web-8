2f785d004990f0c13a7437d66bd458d9a82556ea
RifkyCahyoPutra <rifkycahyo239@gmail.com>
2024-11-19 16:41:18 +0700

    first commit

---
+++ Pertemuan 7/tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1E3A8A',
+        secondary: '#FBBF24',
+        neutral: '#F3F4F6',
+        
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ Pertemuan 9/tailwind-layout/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  darkmode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF', // Warna biru khusus
+        secondary: '#FBBF24', // Warna kuning khusus
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
